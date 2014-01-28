1c3e73668c828f483dce54ba3219db5289012f9b
Yasuyuki Oka <yasuyk@gmail.com>
2014-01-28 21:36:54 +0900

    Add Package-Requires header

--- mip-mode.el
+++ mip-mode.el
@@ -4,6 +4,7 @@
 
 ;; Author: Eeli Reilin <eeli.reilin@gmail.com>
 ;; Keywords: workspaces workspace project projects mip-mode
+;; Package-Requires: ((dash "2.5.0"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
