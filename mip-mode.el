ab82a49cf04a83f36163fb823f2a7039b60b7cb5
gaudecker <gaudecker@users.noreply.github.com>
2014-10-23 14:50:02 +0300

    Removed dash from package requirements.

--- mip-mode.el
+++ mip-mode.el
@@ -4,7 +4,6 @@
 
 ;; Author: Eeli Reilin <gaudecker@fea.st>
 ;; Keywords: workspaces workspace project projects mip-mode
-;; Package-Requires: ((dash "2.5.0"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
